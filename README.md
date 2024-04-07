9ba1a1175f204a420bb4f67480accde25934d393
Cara Townsend <55190235+townsece@users.noreply.github.com>
2024-04-07 22:29:45 +0100

    Re-organise videos after removal (#2)
    * tidy order after remove
    
    * wipe the shorts
    
    * update info/docs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hideyt
-Quick and dirty add-on for firefox to vanish watched videos when extension is loaded.
+
+Quick and dirty add-on for firefox to vanish watched videos when extension is clicked in toolbar.
+
+## How to install
+
+Download or clone the repository, then follow the instructions
+at https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension#installing to apply to
+your browser. The file you should select is the manifest.json.
+
+## How to run
+
+Once installed, the extension should show up in your toolbar. Just click the icon while a YouTube tab is loaded to apply
+the watched filter. To remove the filter, refresh the page.
--- hideyt.js
+++ hideyt.js
@@ -1,10 +1,40 @@
-let cssPath = "#contents #content"
+let thumbnailPath = "#contents #content"
+let containerPath = "#contents ytd-rich-item-renderer"
+let shortsXPath = "//div[contains(@class, \"section\") and @id=\"content\" and .//*[contains(text(),\"Shorts\")]]/.."
 
 browser.runtime.onMessage.addListener((request) => {
-    document.body.querySelectorAll(cssPath).forEach(el => {
+    // wipe out the shorts rail entirely
+    // TODO - resolve issue with disappearing videos and reappearing shorts rail on scroll-to-load-more
+    document.evaluate(shortsXPath, document).iterateNext().remove()
+    // remove all videos which have the progress bar inlaid
+    document.body.querySelectorAll(thumbnailPath).forEach(el => {
         if (el.querySelectorAll(".ytd-thumbnail-overlay-resume-playback-renderer").length > 0) {
             el.remove()
         }
     })
+    let boxes = document.body.querySelectorAll(containerPath)
+    let rawVideos = document.body.querySelectorAll(thumbnailPath)
+    let videos = []
+    // ignore non-videos like the shorts rail and header
+    rawVideos.forEach(el => {
+        if (el.checkVisibility() && el.querySelectorAll("div#details").length > 0 && el.querySelectorAll("#menu-container").length === 0) {
+            videos.push(el)
+        }
+    })
+    let hitRemoved = false
+    for (let i = 0; i < videos.length; i++) {
+        if (!hitRemoved) {
+            // Once we hit the first empty container, we are just backfilling, regardless of existing videos
+            hitRemoved = boxes[i].querySelectorAll("#content").length === 0;
+        }
+        if (hitRemoved) {
+            console.log("hit removed at index " + i)
+            // Remove the existing video from the dom and insert our own
+            if (boxes[i].querySelectorAll("#content").length > 0) {
+                boxes[i].querySelector("#content").remove()
+            }
+            boxes[i].insertAdjacentElement("afterbegin", videos[i])
+        }
+    }
     return Promise.resolve({ response: "success" });
 });
