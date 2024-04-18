d212b4967cd488b92a4b608e5e75d1a3ada5f54f
Cara Townsend <townsendcara98@gmail.com>
2024-04-18 22:56:04 +0100

    fix data passing

--- background.js
+++ background.js
@@ -1,13 +1,22 @@
+let defaultSettings = {hideUpcoming : false}
+
 browser.browserAction.onClicked.addListener(
     (tab) => {
-        browser.storage.local.get().then(removeWatched, handleError)
+        browser.storage.local.get().then(setSettings, handleError)
+        removeWatched(tab, defaultSettings)
     });
 
-function removeWatched(storedSettings) {
+function setSettings(storedSettings) {
+    let fromStored = storedSettings.hideUpcoming
+    console.log("Also removing upcoming videos? " + fromStored)
+    defaultSettings.hideUpcoming = fromStored
+}
+
+function removeWatched(tab) {
     console.log("Removing watched from tab")
     browser.tabs.sendMessage(
         tab.id,
-        {request: "Init remove"}
+        { remove_upcoming: defaultSettings.hideUpcoming }
     ).then((response) => {
         console.log("Got: " + response.response);
     }).catch(() => console.warn("Extension not running or not on Youtube tab"));
