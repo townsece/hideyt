12c52bcae37e5c4949d3afceda9aa83cf242cc71
Cara Townsend <townsendcara98@gmail.com>
2024-04-18 21:15:11 +0100

    store options to local storage

--- options/options.js
+++ options/options.js
@@ -1,7 +1,9 @@
-async function saveOptions(e) {
-    e.preventDefault()
+function saveOptions() {
     let hideUpcoming = document.querySelector("#upcoming").checked
     console.log("Got hide upcoming as: " + hideUpcoming)
+    browser.storage.local.set({
+        hideUpcoming
+    });
 }
 
 document.querySelector("form").addEventListener("submit", saveOptions)
