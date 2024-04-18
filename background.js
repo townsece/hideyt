3c3acf97402b283ebfb92eb5fda48100d501d10c
Cara Townsend <townsendcara98@gmail.com>
2024-04-18 21:40:19 +0100

    extract out message to content script to storage

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
 browser.browserAction.onClicked.addListener(
     (tab) => {
-        console.log("Removing watched from tab")
-        browser.tabs.sendMessage(
-            tab.id,
-            {request: "Init remove"}
-        ).then((response) => {
-            console.log("Got: " + response.response);
-        }).catch(() => console.warn("Extension not running or not on Youtube tab"));
+        browser.storage.local.get().then(removeWatched, handleError)
     });
+
+function removeWatched(storedSettings) {
+    console.log("Removing watched from tab")
+    browser.tabs.sendMessage(
+        tab.id,
+        {request: "Init remove"}
+    ).then((response) => {
+        console.log("Got: " + response.response);
+    }).catch(() => console.warn("Extension not running or not on Youtube tab"));
+
+}
+
+function handleError(e) {
+    console.error(e)
+}
