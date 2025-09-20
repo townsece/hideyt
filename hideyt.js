d573bc03e5d226942394436a407f0a606fcab10a
Cara Townsend <townsendcara98@gmail.com>
2025-09-20 21:53:51 +0100

    tweaks and fixes

--- hideyt.js
+++ hideyt.js
@@ -1,44 +1,59 @@
-const thumbnailPath = "#contents ytd-rich-item-renderer #content"
-const containerPath = "#contents ytd-rich-item-renderer"
-const resumeOverlayPath = ".ytd-thumbnail-overlay-resume-playback-renderer"
-const resumeOverlayPath_new = ".ytThumbnailOverlayProgressBarHostWatchedProgressBarSegment"
-const upcomingOverlayPath = "#time-status [aria-label=\"Upcoming\"]"
-const shortsXPath = "//div[contains(@class, \"section\") and @id=\"content\" and .//*[contains(text(),\"Shorts\")]]/.."
+const thumbnailPath = "#contents ytd-rich-item-renderer #content";
+const containerPath = "#contents ytd-rich-item-renderer";
+const resumeOverlayPath = ".ytd-thumbnail-overlay-resume-playback-renderer";
+const resumeOverlayPath_new = ".ytThumbnailOverlayProgressBarHostWatchedProgressBarSegment";
+const upcomingOverlayPath = "#time-status [aria-label=\"Upcoming\"]";
+const shortsXPath = "//div[contains(@class, \"section\") and @id=\"content\" and .//*[contains(text(),\"Shorts\")]]/..";
 
-browser.runtime.onMessage.addListener((request) => {
+function orchestrateRemoveWatched (request) {
+
     // wipe out the shorts rail entirely if exists
-    document.evaluate(shortsXPath, document).iterateNext()?.remove()
+    document.evaluate(shortsXPath, document).iterateNext()?.remove();
 
-    let selectorsToRemove = [resumeOverlayPath, resumeOverlayPath_new]
+    let selectorsToRemove = [resumeOverlayPath, resumeOverlayPath_new];
     if (request.hideUpcoming) {
-        selectorsToRemove.push(upcomingOverlayPath)
+        selectorsToRemove.push(upcomingOverlayPath);
     }
+
+    let removed = [];
     // remove all videos which have the progress bar inlaid
     document.body.querySelectorAll(thumbnailPath).forEach(el => {
         for (let path of selectorsToRemove) {
             if (el.querySelectorAll(path).length > 0) {
-                el.remove()
-                break
+                el.remove();
+                removed.push(el);
+                break;
             }
         }
     })
-    let boxes = document.body.querySelectorAll(containerPath)
-    let rawVideos = document.body.querySelectorAll(thumbnailPath)
-    let videos = []
-    // ignore non-videos like the shorts rail and header
+    let boxes = document.body.querySelectorAll(containerPath);
+    let rawVideos = document.body.querySelectorAll(thumbnailPath);
+    let videos = [];
+    // filter out non-videos like the shorts rail and header
     rawVideos.forEach(el => {
-        if (el.checkVisibility() && el.querySelectorAll("div#details").length > 0 && el.querySelectorAll("#menu-container").length === 0) {
-            videos.push(el)
+        if (el.checkVisibility() && el.querySelectorAll("#menu-container").length === 0) {
+            videos.push(el);
         }
     })
 
     for (let i = 0; i < videos.length; i++) {
-        console.log("hit removed at index " + i)
+        console.log("hit removed at index " + i);
         // Remove the existing video from the dom and insert our own (which could be the same video)
         if (boxes[i].querySelectorAll("#content").length > 0) {
-            boxes[i].querySelector("#content").remove()
+            boxes[i].querySelector("#content").remove();
         }
-        boxes[i].insertAdjacentElement("afterbegin", videos[i])
+        boxes[i].insertAdjacentElement("afterbegin", videos[i]);
     }
-    return Promise.resolve({response: "success"})
+}
+
+browser.runtime.onMessage.addListener((request) => {
+    // Ensure everything has finished loading before firing
+    if (document.readyState === 'loading' || document.readyState === 'interactive') {
+        document.addEventListener("DOMContentLoaded", () => {
+            orchestrateRemoveWatched(request);
+        })
+    } else {
+        orchestrateRemoveWatched(request);
+    }
+    return Promise.resolve({ response: "success" });
 })
