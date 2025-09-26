422c499f5be5a4cca1abe7c8645dd848a82df16c
Cara Townsend <townsendcara98@gmail.com>
2025-09-26 12:35:59 +0100

    Allow setting number of columns

--- hideyt.js
+++ hideyt.js
@@ -5,7 +5,9 @@ const resumeOverlayPath_new = ".ytThumbnailOverlayProgressBarHostWatchedProgress
 const upcomingOverlayPath = "#time-status [aria-label=\"Upcoming\"]";
 const shortsXPath = "//div[contains(@class, \"section\") and @id=\"content\" and .//*[contains(text(),\"Shorts\")]]/..";
 
-function orchestrateRemoveWatched (request) {
+const gridBox = "ytd-rich-grid-renderer";
+
+function orchestrateRemoveWatched(request) {
 
     // wipe out the shorts rail entirely if exists
     document.evaluate(shortsXPath, document).iterateNext()?.remove();
@@ -34,15 +36,34 @@ function orchestrateRemoveWatched (request) {
         if (el.checkVisibility() && el.querySelectorAll("#menu-container").length === 0) {
             videos.push(el);
         }
-    })
+    });
 
-    for (let i = 0; i < videos.length; i++) {
-        console.log("hit removed at index " + i);
-        // Remove the existing video from the dom and insert our own (which could be the same video)
-        if (boxes[i].querySelectorAll("#content").length > 0) {
-            boxes[i].querySelector("#content").remove();
+    if (request.applyCustomColumns) {
+        console.log(`setting columns to ${request.requestedColumns}`);
+        let requestedColumnsString = String(request.requestedColumns);
+        let flexStyle = document.querySelector(gridBox).getAttribute("style");
+        flexStyle = flexStyle.replaceAll(/([a-zA-Z0-9\-]+): \d;/gi, `$1: ${requestedColumnsString};`);
+        document.querySelector(gridBox).setAttribute("style", flexStyle);
+        for (let i = 0; i < videos.length; i++) {
+            // Remove the existing video from the dom and insert our own (which could be the same video)
+            boxes[i].setAttribute("items-per-row", requestedColumnsString);
+            if (boxes[i].querySelectorAll("#content").length > 0) {
+                boxes[i].querySelector("#content").remove();
+            }
+            boxes[i].insertAdjacentElement("afterbegin", videos[i]);
         }
-        boxes[i].insertAdjacentElement("afterbegin", videos[i]);
+        for (let i = videos.length; i < boxes.length; i++) {
+            // Set for remaining boxes
+            boxes[i].setAttribute("items-per-row", requestedColumnsString);
+        }
+    } else {
+        for (let i = 0; i < videos.length; i++) {
+            // Remove the existing video from the dom and insert our own (which could be the same video)
+            if (boxes[i].querySelectorAll("#content").length > 0) {
+                boxes[i].querySelector("#content").remove();
+            }
+            boxes[i].insertAdjacentElement("afterbegin", videos[i]);
+        }
     }
 }
 
--- options/options.js
+++ options/options.js
@@ -1,13 +1,21 @@
 function saveOptions() {
     let hideUpcoming = document.querySelector("#upcoming").checked
     console.log("Got hide upcoming as: " + hideUpcoming)
+    let requestedColumns = document.querySelector("#thumbnail-row-width").value
+    let applyCustomColumns = document.querySelector("#apply-row-width").checked
+    console.log("Got custom columns as: " + requestedColumns)
+    console.log(`${applyCustomColumns ?? "not "}applying custom columns`)
     browser.storage.local.set({
-        hideUpcoming
+        hideUpcoming,
+        requestedColumns,
+        applyCustomColumns
     });
 }
 
 function retrieveOptions(optionsFromStorage) {
     document.querySelector("#upcoming").checked = optionsFromStorage.hideUpcoming
+    document.querySelector("#thumbnail-row-width").value = optionsFromStorage.requestedColumns
+    document.querySelector("#apply-row-width").checked = optionsFromStorage.applyCustomColumns
 }
 
 function handleError(e) {
